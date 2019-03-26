695cc241e082059240f640543e2565337f96ba34
NurrofiqiAnkisqiantari <nurrofiqi.2310@students.amikom.ac.id>
2019-03-26 14:56:47 +0700

    LatihanMemberClass

---
+++ LatihanMemberClass/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek
+            Taxi taxi = new Taxi();
+
+            // pengeset nilai properties
+            taxi.DriveName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            // pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickupPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
